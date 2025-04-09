18f85e52ae5ded4a7f20e6d7e248cfcaf7b32cfe
upcteus <u202210059@upc.edu.pe>
2025-04-09 00:00:06 -0500

    feat: add Coming Soon page and UI adjustments in header and sidebar
    -Created the Coming Soon page under the public-page bounded context
    
    -Removed the search bar and calendar logo from the header for a cleaner UI
    
    -Added new routes to the sidebar to reflect updated navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,15 @@
 import { Routes } from '@angular/router';
 import {AuthenticationComponent} from "./iam/pages/authentication/authentication.component";
 import {ConfirmEmailComponent} from "./iam/pages/authentication/confirm-email/confirm-email.component";
-import {LockScreenComponent} from "./iam/pages/authentication/lock-screen/lock-screen.component";
 import {LogoutComponent} from "./iam/pages/authentication/logout/logout.component";
 import {ResetPasswordComponent} from "./iam/pages/authentication/reset-password/reset-password.component";
 import {SignInComponent} from "./iam/pages/authentication/sign-in/sign-in.component";
-import {SignUpComponent} from "./iam/pages/authentication/sign-up/sign-up.component";
 import {ForgotPasswordComponent} from "./iam/pages/authentication/forgot-password/forgot-password.component";
 import {NotFoundComponent} from "./public/pages/not-found/not-found.component";
 import {InternalErrorComponent} from "./public/pages/internal-error/internal-error.component";
 import {BlankPageComponent} from "./public/pages/blank-page/blank-page.component";
 import {BlankLayoutComponent} from "./shared/layouts/blank-layout/blank-layout.component";
 import {DashboardComponent} from "./dashboard/pages/dashboard.component";
-import {AppComponent} from "./app.component";
 import {MainLayoutComponent} from "./shared/layouts/main-layout/main-layout.component";
 import {SettingsComponent} from "./settings/pages/settings.component";
 import {PrivacyPolicyComponent} from "./settings/pages/privacy-policy/privacy-policy.component";
@@ -30,6 +27,7 @@ import {ChildrenComponent} from "./children/pages/children.component";
 import {CreateChildComponent} from "./children/pages/create-child/create-child.component";
 import {EditChildComponent} from "./children/pages/edit-child/edit-child.component";
 import {FaqPageComponent} from "./public/pages/faq-page/pages/faq-page.component";
+import {ComicSoonComponent} from "./public/pages/comic-soon/comic-soon.component";
 
 export const routes: Routes = [
   { path: '',redirectTo: 'authentication', pathMatch: 'full' },
@@ -42,10 +40,8 @@ export const routes: Routes = [
         children: [
           {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
           {path: 'sign-in', component: SignInComponent},
-          {path: 'sign-up', component: SignUpComponent},
           {path: 'forgot-password', component: ForgotPasswordComponent},
           {path: 'reset-password', component: ResetPasswordComponent},
-          {path: 'lock-screen', component: LockScreenComponent},
           {path: 'confirm-email', component: ConfirmEmailComponent},
           {path: 'logout', component: LogoutComponent},
         ]
@@ -94,6 +90,8 @@ export const routes: Routes = [
           { path: 'edit-child', component: EditChildComponent},
         ]
       },
+      { path: 'my-profile', component: ComicSoonComponent},
+      { path: 'comic-soon', component: ComicSoonComponent},
       { path: '**', component: NotFoundComponent },
     ]
   },
