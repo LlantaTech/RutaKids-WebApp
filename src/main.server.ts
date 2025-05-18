2790d0b4e313c90f0b4d3684a932d22a0b395c8f
upcteus <u202210059@upc.edu.pe>
2025-05-18 02:09:20 -0500

    puta

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+import 'zone.js/node';
+import 'source-map-support/register';
+
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
