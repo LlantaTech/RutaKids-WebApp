41ced739c6b323ba0cbbe88d95520374ead69394
upcteus <u202210059@upc.edu.pe>
2025-05-18 01:29:20 -0500

    pene

--- src/app/dashboard/pages/projects-overview/completed-projects/completed-projects.component.ts
+++ src/app/dashboard/pages/projects-overview/completed-projects/completed-projects.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
 import { CustomizerSettingsService } from '../../../../shared/services/customizer-settings/customizer-settings.service';
 import { PlatformService } from '../../../../shared/services/platform/platform.service';
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-completed-projects',
   standalone: true,
-  imports: [MatCardModule, RouterLink, NgOptimizedImage],
+  imports: [MatCardModule],
   templateUrl: './completed-projects.component.html',
   styleUrls: ['./completed-projects.component.scss']
 })
--- src/app/public/pages/internal-error/internal-error.component.ts
+++ src/app/public/pages/internal-error/internal-error.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
     selector: 'app-internal-error',
     standalone: true,
-  imports: [RouterLink, MatCardModule, MatButtonModule, NgOptimizedImage],
+  imports: [RouterLink, MatCardModule, MatButtonModule],
     templateUrl: './internal-error.component.html',
     styleUrls: ['./internal-error.component.scss']
 })
